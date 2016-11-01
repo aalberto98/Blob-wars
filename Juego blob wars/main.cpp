de98714bb02f6719d9b023df9336a45445bf70f6
Alberto <aji147@gmail.com>
2016-11-01 17:01:50 -0600

    Initial Commit

---
+++ Juego blob wars/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Juego blob wars
+//
+//  Created by Alberto Juarez on 11/1/16.
+//  Copyright © 2016 Alberto Juarez. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
